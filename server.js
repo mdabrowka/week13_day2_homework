fc86199e1ab5cbcc05ff8d2c9f12e16c3fed1080
Marta <martadabrowka@gmail.com>
2017-12-19 16:17:34 +0000

    created a database and established a connection

--- server.js
+++ server.js
@@ -2,3 +2,17 @@ const express = require('express');
 const server = express();
 const parser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+
+
+server.use(parser.json());
+server.use(express.static('client/build'));
+server.use(parser.urlencoded({extended: true}));
+
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+  if(err) {
+    console.log(err);
+    return;
+  }
+  const db = client.db("potatoes");
+  console.log("Connected to database");
+})
